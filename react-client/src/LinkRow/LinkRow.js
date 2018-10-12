3da48ffa6a6a3a46bb7daeb4442a4d5cbffe6d27
Svetoslav Videnov <vauvenal5@gmail.com>
2018-10-12 08:44:51 +0200

    Add action for select button

--- react-client/src/LinkRow/LinkRow.js
+++ react-client/src/LinkRow/LinkRow.js
@@ -4,7 +4,7 @@ import { Label, Table, Button } from 'semantic-ui-react';
 import TagView from '../TagView';
 import * as actions from '../modelActions';
 
-export const LinkRow = ({types, link, linkSelect}) => {
+export const LinkRow = ({types, link, linkSelect, contractSelect}) => {
     
     let tags = link.tags.map(
         (tagId) => <TagView key={tagId} tag={types[tagId]}/>
@@ -24,8 +24,8 @@ export const LinkRow = ({types, link, linkSelect}) => {
                 {tags}
             </Table.Cell>
             <Table.Cell>
-                    <Button icon='info' content="Info" labelPosition='left'/>
-                    <Button icon='eye' content="Select" labelPosition='left'/>
+                    {/* <Button icon='info' content="Info" labelPosition='left'/> */}
+                    <Button onClick={() => contractSelect(link.getAddress())} icon='eye' content="Select" labelPosition='left'/>
             </Table.Cell>
         </Table.Row>
     );
@@ -33,7 +33,11 @@ export const LinkRow = ({types, link, linkSelect}) => {
 
 export const mapDispatchToProps = (dispatch) => {
     return {
-        linkSelect: (address) => dispatch(actions.onLinkSelect(address))
+        linkSelect: (address) => dispatch(actions.onLinkSelect(address)),
+        contractSelect: (address) => {
+            dispatch(actions.onContractLoad(address));
+            dispatch(actions.onContractSelect(address));
+        }
     }
 }
 
